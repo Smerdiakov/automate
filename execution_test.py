bbf232bbb91e36c706023af8d20e60f4bb147ed6
Smerdiakov <lugocadi@yahoo.fr>
2015-01-03 16:27:24 +0100

    tests unitaires pour la classe execution

--- execution_test.py
+++ execution_test.py
@@ -45,7 +45,7 @@ auto_eps.ajoute_transition(4,4,"a")
 auto_eps.ajoute_epsilon(1,2)
 auto_eps.ajoute_epsilon(3,4)
 
-longueur_mots = 900
+longueur_mots = 950
 
 def mot_alea (liste_bits):
 	mot = ""
@@ -97,24 +97,99 @@ def liste_propriete_3(long):
 				
 class test_execution (unittest.TestCase):
 
-	def test_auto_det (self):
-		for test in range(100):
+	# tests unitaires
+	
+	def test_auto_det_unit(self):
+		mot_1 = "abbaba"
+		mot_2 = "abbabb"
+		mot_3 = "abbabc"
+		
+		execut = execution(auto_det)
+		execut.execute(mot_1)
+		self.assertTrue(execut.bool)
+		self.assertEqual(execut.suite_etats,[1,2,2,2,3,3,1])
+		self.assertEqual(execut.suite_epsilon,[0,0,0,0,0,0])
+		
+		execut.execute(mot_2)
+		self.assertFalse(execut.bool)
+		
+		execut.execute(mot_3)
+		self.assertFalse(execut.bool)
+	
+	def test_auto_ndet_unit(self):
+		mot_1 = "abbaab"
+		mot_2 = "abbaba"
+		mot_3 = "abbabb"
+		mot_4 = "abbabc"
+		
+		execut = execution(auto_ndet)
+		
+		execut.execute(mot_1)
+		self.assertTrue(execut.bool)
+		self.assertEqual(execut.suite_etats,[1,1,1,1,1,2,3])
+		self.assertEqual(execut.suite_epsilon,[0,0,0,0,0,0])
+		
+		execut.execute(mot_2)
+		self.assertTrue(execut.bool)
+		self.assertEqual(execut.suite_etats,[1,1,1,1,1,1,2])
+		self.assertEqual(execut.suite_epsilon,[0,0,0,0,0,0])
+		
+		execut.execute(mot_3)
+		self.assertFalse(execut.bool)
+	
+		execut.execute(mot_4)
+		self.assertFalse(execut.bool)
+		
+		
+	def test_auto_eps_unit(self):
+		mot_1 = "aaabbb"
+		mot_2 = "bbaaaa"
+		mot_3 = "abbaba"
+		mot_4 = "aacbbb"
+		
+		execut = execution(auto_eps)
+		
+		execut.execute(mot_1)
+		self.assertTrue(execut.bool)
+		self.assertEqual(execut.suite_etats,[1,1,1,1,2,2,2,2])
+		self.assertEqual(execut.suite_epsilon,[0,0,0,1,0,0,0])
+		
+		execut.execute(mot_2)
+		self.assertTrue(execut.bool)
+		self.assertEqual(execut.suite_etats,[3,3,3,4,4,4,4,4])
+		self.assertEqual(execut.suite_epsilon,[0,0,1,0,0,0,0])
+		
+		execut.execute(mot_3)
+		self.assertFalse(execut.bool)
+	
+		execut.execute(mot_4)
+		self.assertFalse(execut.bool)
+	
+	
+	# tests aleatoires
+
+	def test_auto_det_alea (self):
+		for test in range(33*5):
+			if test%5==0:
+				print(" --> ",int(test/5)," % effectués.")
 			liste = liste_bits_alea(longueur_mots)
 			mot = mot_alea(liste)
 			nombre = nombre_de_zeros_mod_3(liste)
-			execut = execution(auto_det,mot)
-			execut.execute()
+			execut = execution(auto_det)
+			execut.execute(mot)
 			if execut.bool :
 				self.assertEqual(nombre,0)
 			else:
 				self.assertNotEqual(nombre,0)
 
-	def test_auto_ndet (self):
-		for test in range(100):
+	def test_auto_ndet_alea (self):
+		for test in range(66*5,101*5):
+			if test%5==0:
+				print(" --> ",int(test/5)," % effectués.")
 			liste = liste_bits_alea(longueur_mots)
 			mot = mot_alea(liste)
-			execut = execution(auto_ndet,mot)
-			execut.execute()
+			execut = execution(auto_ndet)
+			execut.execute(mot)
 			if (((liste[longueur_mots-1]==0)or((liste[longueur_mots-1]==1)and(liste[longueur_mots-2]==0))) == execut.bool):
 				pass
 			else : 
@@ -122,16 +197,18 @@ class test_execution (unittest.TestCase):
 				print(mot)
 				self.assertTrue(((liste[longueur_mots-1]==0)or((liste[longueur_mots-1]==1)and(liste[longueur_mots-1]==1))) == execut.bool)
 	
-	def test_auto_eps(self):
-		for test in range(100):
+	def test_auto_eps_alea(self):
+		for test in range(33*5,66*5):
+			if test%5==0:
+				print(" --> ",int(test/5)," % effectués.")
 			liste = []
 			if random.randint(0,1)==1:			
 				liste = liste_bits_alea(longueur_mots)
 			else:
 				liste = liste_propriete_3(longueur_mots)
 			mot = mot_alea(liste)
-			execut = execution(auto_eps,mot)
-			execut.execute()
+			execut = execution(auto_eps)
+			execut.execute(mot)
 			if (((propriete_3(mot,"a"))or(propriete_3(mot,"b"))) == execut.bool):
 				pass
 			else : 
