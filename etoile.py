92e708f29481b2a9c471497dc8d6ef155aa39c3d
Ludovic <lugocadi@yahoo.fr>
2015-01-30 11:46:09 +0100

    execution des tests
    quelques modifications dues à des erreurs dans la construction du test.

--- etoile.py
+++ etoile.py
@@ -14,37 +14,37 @@ def etoile (auto):
 	nouvel_auto = auto;
 	
 	# un rajoute un nouvel etat 0 (ou autre si 0 est déja pris)
-	nouvel_etat = etiquette_nouvel_etat(nouvel_auto);
-	nouvel_auto.ajoute_etat(nouvel_etat);
+	nouvel_etat = etiquette_nouvel_etat(nouvel_auto)
+	nouvel_auto.ajoute_etat(nouvel_etat)
 	
 	# on rajoute des epsilon transitions 0 --> i  pour tout i état initial de l'automate
-	ajoute_transitions_initiales(nouvel_auto, nouvel_etat);
+	ajoute_transitions_initiales(nouvel_auto, nouvel_etat)
 	
 	# on rajoute des epsilon transitions j --> 0 pour tout j état final de l'automate
-	ajoute_transitions_finales(nouvel_auto, nouvel_etat);
+	ajoute_transitions_finales(nouvel_auto, nouvel_etat)
 	
 	# on transforme 0 en état initial et final.
-	nouvel_auto.ajoute_initial(nouvel_etat);
-	nouvel_auto.ajoute_final(nouvel_etat);
+	nouvel_auto.ajoute_initial(nouvel_etat)
+	nouvel_auto.ajoute_final(nouvel_etat)
 	
 	return nouvel_auto;
 	
-def etiquette_nouvel_etat(auto);
-	liste_etat = auto.liste_etats();
-	nouvel_etat = 0;
+def etiquette_nouvel_etat(auto):
+	liste_etat = auto.liste_etats()
+	nouvel_etat = 0
 	
 	# si 0 est déjà dans l'automate, on essaie un autre nombre.
 	while (nouvel_etat in liste_etat):
-		nouvel_etat = (nouvel_etat+1)*5; 
+		nouvel_etat = (nouvel_etat+1)*5
 		
-	return liste_etat;
+	return nouvel_etat
 	
 	
 def ajoute_transitions_initiales(nouvel_auto, nouvel_etat):
 	for etat_initial in nouvel_auto.initial:
-		nouvel_auto.ajoute_epsilon(nouvel_etat, etat_initial);
+		nouvel_auto.ajoute_epsilon(nouvel_etat, etat_initial)
 		
 def ajoute_transitions_finales(nouvel_auto, nouvel_etat):
 	for etat_final in nouvel_auto.final:
-		nouvel_auto.ajoute_epsilon(etat_final, nouvel_etat);
+		nouvel_auto.ajoute_epsilon(etat_final, nouvel_etat)
 		
--- etoile_test.py
+++ etoile_test.py
@@ -4,7 +4,7 @@
 
 from execution import *
 from automate import *
-from concatenation import *
+from etoile import *
 import unittest
 import random
 
@@ -29,17 +29,18 @@ def liste_bits_alea (taille) :
 	return liste_bits
 	
 
-# cet automate non déterministe reconnait les mots qui se terminent par a ou par ab
-auto_ndet = automate()
-auto_ndet.ajoute_initial(1)
-auto_ndet.ajoute_final(2)
-auto_ndet.ajoute_final(3)
-auto_ndet.ajoute_transition(1,2,"a")
-auto_ndet.ajoute_transition(1,1,"a")
-auto_ndet.ajoute_transition(1,1,"b")
-auto_ndet.ajoute_transition(2,3,"b")
+auto = automate()
+auto.ajoute_initial(1)
+auto.ajoute_final(2)
+auto.ajoute_final(3)
+auto.ajoute_transition(1,2,"a")
+auto.ajoute_transition(2,3,"a")
+auto.ajoute_transition(3,1,"a")
+auto.ajoute_transition(1,1,"b")
+auto.ajoute_transition(2,2,"b")
+auto.ajoute_transition(3,3,"b")
 
-auto_etoile = etoile(auto_ndet)
+auto_etoile = etoile(auto)
 
 class test_etoile(unittest.TestCase):
 	def test_etoile(self):
@@ -50,38 +51,39 @@ class test_etoile(unittest.TestCase):
 		
 			liste_1 = liste_bits_alea(longueur_mots)
 			mot_1 = mot_alea(liste_1)
-			execut_1 = execution(auto_ndet)
+			execut_1 = execution(auto)
 			execut_1.execute(mot_1)
 			
 			liste_2 = liste_bits_alea(longueur_mots)
 			mot_2 = mot_alea(liste_2)
-			execut_2 = execution(auto_ndet)
+			execut_2 = execution(auto)
 			execut_2.execute(mot_2)
 			
 			liste_3 = liste_bits_alea(longueur_mots)
 			mot_3 = mot_alea(liste_3)
-			execut_3 = execution(auto_ndet)
+			execut_3 = execution(auto)
 			execut_3.execute(mot_3)
 			
 			liste_4 = liste_bits_alea(longueur_mots)
 			mot_4 = mot_alea(liste_4)
-			execut_4 = execution(auto_ndet)
+			execut_4 = execution(auto)
 			execut_4.execute(mot_4)
 			
 			liste_5 = liste_bits_alea(longueur_mots)
 			mot_5 = mot_alea(liste_5)
-			execut_5 = execution(auto_ndet)
+			execut_5 = execution(auto)
 			execut_5.execute(mot_5)
 		
 			execut_etoile = execution(auto_etoile)
 			execut_etoile.execute(mot_1+mot_2+mot_3+mot_4+mot_5)
 		
 		# on vérifie si la propriété de concatenation est vérifiée :
-			if (execut_1.bool and execut_2.bool and execut_3.bool and execut_4.bool and execut_5.bool)==(execut_concat.bool) :
-				pass
-			else :
-				print("la concatenation de fonctionnne pas pour les mots :\n",mot_1,"\n", mot_2,"\n", mot_3,"\n", mot_4,"\n", mot_5)
-			self.assertEqual( execut_det.bool and execut_ndet.bool, execut_concat.bool)
+			if (execut_1.bool and execut_2.bool and execut_3.bool and execut_4.bool and execut_5.bool):
+				if(execut_etoile.bool):
+					pass
+				else :
+					print("la concatenation de fonctionnne pas pour les mots :\n",mot_1,"\n", mot_2,"\n", mot_3,"\n", mot_4,"\n", mot_5)
+				self.assertTrue(execut_etoile.bool)
 			
 if __name__=="__main__":
 	print("\n")
